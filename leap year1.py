76109c1f0031c0c05f38fb67bd0fcad94904fa79
shivam parashar <77877958+9719966914@users.noreply.github.com>
2021-03-17 02:09:10 -0700

    Add files via upload

---
+++ leap year1.py
@@ -0,0 +1,9 @@
+year=eval(input('enter the year'))
+if year % 4==0:
+    if year % 100 !=0:
+          print('leap year')
+    else:
+        if year % 400==0:
+            print('leap year')
+else:
+    print('not leap year')
