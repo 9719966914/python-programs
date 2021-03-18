4aa800d3d5488d5a3f96a20611718bc68698360c
shivam parashar <77877958+9719966914@users.noreply.github.com>
2021-03-18 16:33:31 +0530

    Add files via upload

---
+++ upper lower digit character.py
@@ -0,0 +1,11 @@
+ch=input('enter anything')
+if ch>='A' and ch<='Z':
+    print('upper case')
+elif ch>='a' and ch<='z':
+    print('lower case')
+elif ch>='0' and ch<='9':
+    print('digits')
+else:
+    print('special character')
+    
+    
